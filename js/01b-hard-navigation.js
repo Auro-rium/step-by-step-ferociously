a4b6df9d6d28d2649293396c38ed53a6a36d3f60
Aurorium Nexus <ishtrivedi18@gmail.com>
2026-08-01 19:01:07 +0530

    Remove legacy FINISH frontend and stage clean TypeScript rebuild

--- js/01b-hard-navigation.js
+++
@@ -1,5 +0,0 @@
-navigate = function navigateWithFullPageLoad(path, replace = false) {
-  const target = internalPath(path, '/');
-  if (replace) window.location.replace(target);
-  else window.location.assign(target);
-};
